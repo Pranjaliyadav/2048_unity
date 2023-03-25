233561f33f750552534f0bea6da26de2e5402bc5
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2023-03-25 20:06:44 +0530

    animating

--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -13,6 +13,8 @@ public class TileBoard : MonoBehaviour
 
     private List<Tile> tiles;
 
+    private bool waiting; //waiting for the animation to end, so we can move tile again 
+
     private void Awake()
     {
         grid = GetComponentInChildren<TileGrid>();
@@ -39,17 +41,24 @@ public class TileBoard : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
-            MoveTiles(Vector2Int.up, 0, 1, 1, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
-            MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
-        }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
-            MoveTiles(Vector2Int.left, 1, 1, 0, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
-            MoveTiles(Vector2Int.right, grid.width -2, -1, 0, 1);
+        if(!waiting)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MoveTiles(Vector2Int.up, 0, 1, 1, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveTiles(Vector2Int.left, 1, 1, 0, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveTiles(Vector2Int.right, grid.width - 2, -1, 0, 1);
+            }
         }
     }
 
@@ -69,7 +78,7 @@ public class TileBoard : MonoBehaviour
         }
     }
 
-    private void MoveTile(Tile tile, Vector2Int direction)
+    private bool MoveTile(Tile tile, Vector2Int direction)
     {
         //main logic of game
 
@@ -93,7 +102,22 @@ public class TileBoard : MonoBehaviour
         if(newCell != null)
         {
             tile.MoveTo(newCell);
+            return true; //if we moved a tile
+            //waiting = true; //as we are movig piece right now, so we set waiting to true.
+            //StartCoroutine(WaitForChanges());
         }
+
+        return false; //if we didnt move a tile
+    }
+
+    private IEnumerator WaitForChanges()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(0.1f);
+        waiting = false;
+        
+        //todo - create a new tile, check for game over
+
     }
 
 
